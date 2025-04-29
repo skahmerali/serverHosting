ea0cb3bb57184d6cf5a546b5564f428fc46fbb13
JaffarAman <amanjaffar50@gmail.com>
2025-04-29 11:52:59 +0500

    server web hosting

---
+++ Server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/user', (req, res) => {
+    res.send("We are connecting with web server...!")
+});
+
+app.listen(5000, () => console.log('server is running'))
