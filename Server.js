ec2ebb940203f3d100860c913e6e11c97c273bc2
JaffarAman <amanjaffar50@gmail.com>
2025-05-01 11:30:51 +0500

    update key

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const Parse = require("parse")
 const app = express();
-
+Parse.initialize('8n04fTXCp4j0mIOIetLB2qPvMBH5pG8L22S1Krxd', 'dchy6t9vrb98n1LDXE6SoD9CmagksUXSpdoKqTbx');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 app.get('/', (req, res) => {
     res.send("We are connecting with web server...!")
 });
