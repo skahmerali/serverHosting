14c99338217b8263b780f24b6b474eae54c7f9e8
JaffarAman <amanjaffar50@gmail.com>
2025-05-01 11:45:05 +0500

    serverless command added

--- Server.js
+++ Server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const Parse = require("parse")
 const app = express();
+const serverless = require("serverless");
 Parse.initialize('8n04fTXCp4j0mIOIetLB2qPvMBH5pG8L22S1Krxd', 'dchy6t9vrb98n1LDXE6SoD9CmagksUXSpdoKqTbx');
 Parse.serverURL = 'https://parseapi.back4app.com/';
 app.get('/', (req, res) => {
     res.send("We are connecting with web server...!")
 });
 
-app.listen(5000, () => console.log('server is running'))
+app.use("/.netlify/functions/app", '/user');
+module.exports.handler = serverless(app);
+
+app.listen(5000, () => console.log('server is running'));
